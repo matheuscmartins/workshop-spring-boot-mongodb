8945a7423e1f50f82606dc5eb18243a7645d2fde
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2022-12-28 10:02:22 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/javaproject/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.javaproject.workshopmongo.repository;
+
+import com.javaproject.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}

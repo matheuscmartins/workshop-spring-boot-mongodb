2233fa0d05174cd47dc4554f5bed07ac09029644
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2022-12-28 15:16:44 -0300

    Criando Entity Post com User alinhado

---
+++ src/main/java/com/javaproject/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.javaproject.workshopmongo.repository;
+
+import com.javaproject.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
